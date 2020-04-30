91e9f5d742920b5e2c3aff02f6f2e592de0781c8
Shawn Cresante <shawn@cresante.com>
2020-04-30 10:20:02 -0400

    Fix scons files for python3 compatibility and code formatting with black

--- tdm_update/scons_utils.py
+++ tdm_update/scons_utils.py
@@ -1,19 +1,20 @@
-import sys, os, string
+import sys, os
 
 def BuildList( s_prefix, s_string ):
-	s_list = string.split( s_string )
-	for i in range( len( s_list ) ):
-		s_list[i] = os.path.join(s_prefix, s_list[i])
-	return s_list
+    s_list = s_string.split()
+    for i in range( len( s_list ) ):
+        s_list[i] = os.path.join(s_prefix, s_list[i])
+    return s_list
+
 
 def ExecutableName(base_name, os, arch):
-	if os == 'Linux':
-		if arch == 'x86':
-			return base_name + '.linux'
-		elif arch == 'x64':
-			return base_name + '.linux64'
-	elif os == 'FreeBSD':
-		return base_name + '.fbsd'
-	elif os == 'MacOSX':
-		return base_name + '.macosx'
-	return base_name
+    if os == "Linux":
+        if arch == "x86":
+            return base_name + ".linux"
+        elif arch == "x64":
+            return base_name + ".linux64"
+    elif os == "FreeBSD":
+        return base_name + ".fbsd"
+    elif os == "MacOSX":
+        return base_name + ".macosx"
+    return base_name
